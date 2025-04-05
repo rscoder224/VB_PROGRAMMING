d493b3f3e8057f5200c3dc31d36e1428ca8e4403
Shaurya Tiwari <ltsmartofficial@gmail.com>
2025-04-05 22:45:14 +0530

    Update 2VbDataType2.vb
    Byte DataType in Vb

--- 2VbDataType2.vb
+++ 2VbDataType2.vb
@@ -4,7 +4,9 @@ Public Module Program
 
 Public Sub Main(args() As String)
   
-  Dim b As Byte
+  Dim b As Byte   
+                  'Size 8 bit or 1 bit'
+                    'range 0 to 255
   b = 66
   
   Console.WriteLine(Chr(b))
