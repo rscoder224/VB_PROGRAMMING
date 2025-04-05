36146c87d7666464350e317e8798c91172e63acb
Shaurya Tiwari <ltsmartofficial@gmail.com>
2025-04-05 22:51:40 +0530

    Create 3datatype.vb
    Integer Data Type In VB

---
+++ 3datatype.vb
@@ -0,0 +1,23 @@
+' datatypes in VB (Visual Basic Programming)
+
+
+Public Module Program
+
+Public Sub Main(args() As String)
+'Integer DataType In VB'
+
+Dim C as Integer
+
+C = 21
+
+Dim K as Integer
+K = 10
+
+Console.WriteLine(C+K)
+
+
+  
+End Sub
+
+End Module
+
