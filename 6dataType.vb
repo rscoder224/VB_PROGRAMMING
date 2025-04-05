1dedfe9432ac70abf840db443a15a3c1224b7326
Shaurya Tiwari <ltsmartofficial@gmail.com>
2025-04-05 23:02:16 +0530

    Create 6dataType.vb
    Double DataType In Vb Programming

---
+++ 6dataType.vb
@@ -0,0 +1,14 @@
+' datatypes in VB (Visual Basic Programming)
+'Double Data Type In Vb Program'
+
+Public Module Program 
+
+Public Sub Main(args() As String)
+  Dim db As Double
+  
+  db = 12.334555366464
+  Console.WriteLine(db)
+  
+End Sub
+
+End Module
