358ff71185ff58afd742910b30e316f0b3b45919
Shaurya Tiwari <ltsmartofficial@gmail.com>
2025-04-05 22:54:40 +0530

    Create 4DataType.js
    Long DataType in vb Program

---
+++ 4DataType.js
@@ -0,0 +1,17 @@
+' datatypes in VB (Visual Basic Programming)
+'Long datatypes in vb'
+
+Public Module Program
+
+Public Sub Main(args() As String)
+  
+  
+  Dim N as Long
+  
+  N = 12234845
+  
+  Console.WriteLine(N)
+  
+End Sub
+
+End Module
