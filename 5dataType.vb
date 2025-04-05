c67ccaa2860e8589a9f23d148f51b0ded411b85c
Shaurya Tiwari <ltsmartofficial@gmail.com>
2025-04-05 22:58:38 +0530

    Create 5dataType.vb
    Single Data Type For storing Decimal Values In Variables

---
+++ 5dataType.vb
@@ -0,0 +1,19 @@
+' datatypes in VB (Visual Basic Programming)
+
+'Single DataType in vb Programming'
+
+
+Public Module Program
+
+Public Sub Main(args() As String)
+  
+  Dim fs As Single
+  
+  fs = 12.334
+  
+  Console.WriteLine(fs)
+  
+  
+End Sub
+
+End Module
