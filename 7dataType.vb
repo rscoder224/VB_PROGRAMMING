c269646a4918963a811e3975ce5061fab47fbb44
Shaurya Tiwari <ltsmartofficial@gmail.com>
2025-04-05 23:11:09 +0530

    Create 7dataType.vb
    Date DataType In vb Programming

---
+++ 7dataType.vb
@@ -0,0 +1,15 @@
+' datatypes in VB (Visual Basic Programming)
+' Date DataType Can Store Date Inside It
+
+ Public Module Program
+ 
+ Public Sub Main(args() As String)
+
+    Dim a As Date
+    a = Now()
+     
+    Console.WriteLine(a)
+   
+ End Sub
+ 
+ End Module
