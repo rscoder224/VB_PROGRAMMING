1b94dc197e7560979408eedee7a1055bbc3228ea
Shaurya Tiwari <ltsmartofficial@gmail.com>
2025-04-06 10:28:10 +0530

    Create 9DataType.vb
    String DataType in VB programming

---
+++ 9DataType.vb
@@ -0,0 +1,13 @@
+
+' DataType in In visual Basic programming
+
+Public Module Program 
+ Public sub Main(args() As String)
+Dim st As String
+st = "Hello Bro How are You are You Getting Fine"
+
+console.writeLine(st)
+   
+ End sub
+ End Module
+ 
