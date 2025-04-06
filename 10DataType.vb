f7866b99dc06c643f732c4b8b98dec206af3a41a
Shaurya Tiwari <ltsmartofficial@gmail.com>
2025-04-06 10:34:40 +0530

    Create 10DataType.vb
    Object Datatype in Vb Programming

---
+++ 10DataType.vb
@@ -0,0 +1,14 @@
+' Object DataType --> Can Strore all values Boolean, byte, Integer ,Long , Single , Double ,Date Object, Decimal
+' DataType in In visual Basic programming
+
+Public Module Program 
+Public sub Main(args() As String)
+Dim st As Object
+
+st = "Hello Bro How are You are You Getting Fine"
+
+console.writeLine(st)
+   
+ End sub
+ End Module
+ 
