3ea5a3246bc2e1156bd5cbf3562f779514ab7587
Shaurya Tiwari <ltsmartofficial@gmail.com>
2025-04-06 10:25:25 +0530

    Create 8dataType.vb
    Visual Basic Dot net Decimal DataType in Visual Basic Programming

---
+++ 8dataType.vb
@@ -0,0 +1,14 @@
+
+' DataType in In visual Basic programming
+
+Public Module Program 
+ Public sub Main(args() As String)
+ 
+Dim k As Decimal
+k = 32.23
+
+console.WriteLine(k)
+   
+ End sub
+ End Module
+ 
